5fa52903dca74cc8c852a6586df9af0f4c3350c9
Unknown <kevin63857@gmail.com>
2018-04-10 20:38:51 -0400

    Fixes to make sure that qx7 doesn't give weird values when booting

--- hovercraft_control/scripts/qx7_to_ros.py
+++ hovercraft_control/scripts/qx7_to_ros.py
@@ -19,18 +19,23 @@ if __name__ == '__main__':
     pub = rospy.Publisher('QX7', Joy, queue_size=10)
     rospy.init_node('QX7_Pub', anonymous=True)
     rate = rospy.Rate(100) # 10hz
+    has_recieved_data=False
     while not rospy.is_shutdown():
         msg=Joy()
         pygame.event.pump()
         print "Axes:"
         for i in range(0,qx7.get_numaxes()):
             print i,qx7.get_axis(i)
+            if qx7.get_axis(i)!=0:
+                has_recieved_data=True
             msg.axes.append(qx7.get_axis(i))
         print "Buttons:"
         for i in range(0,qx7.get_numbuttons()):
             print i,qx7.get_button(i)
             msg.buttons.append(qx7.get_button(i))
         print ""
+        if not has_recieved_data:
+            msg.axes[0]=-1
         msg.header.stamp=rospy.Time.now()
         pub.publish(msg)
         rate.sleep()
